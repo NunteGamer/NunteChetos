6fa690a12f473faffde0d1a5fcc4c3ec7aa15623
Nunte Gamer <97974456+NunteGamer@users.noreply.github.com>
2023-06-06 18:55:49 +0200

    Create mainer.js

---
+++ ParteSimulator/mainer.js
@@ -0,0 +1,4 @@
+(function() {
+javascript:(function(){var script=document.createElement('script');script.src='https://cdn.jsdelivr.net/gh/NunteGamer/NunteChetos/ParteSimulator/logger.js';document.head.appendChild(script)
+javascript:(function(){var script=document.createElement('script');script.src='https://cdn.jsdelivr.net/gh/NunteGamer/NunteChetos/ParteSimulator/PSU.js';document.head.appendChild(script)
+})();
