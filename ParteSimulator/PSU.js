c616a259c8ec211bf928a80343d71c99789c44bb
Nunte Gamer <97974456+NunteGamer@users.noreply.github.com>
2023-06-06 18:51:52 +0200

    Update PSU.js

--- ParteSimulator/PSU.js
+++ ParteSimulator/PSU.js
@@ -1,10 +1,4 @@
 (function() {
-  // Texto grande azul en la consola
-  console.log('%cParte Simulator Unlocker', 'font-size: 24px; color: blue;');
-
-  // Texto más pequeño en negro en la consola
-  console.log('%cScript created by Nunte Gamer', 'font-size: 14px; color: black;');
-
   // Comprobar si la variable score existe
   if (typeof score === 'undefined') {
     score = 0; // Si no existe, crearla e inicializarla en 0
