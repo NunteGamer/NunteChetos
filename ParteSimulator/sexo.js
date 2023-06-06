65950f98687c47efdf29c63c1dd280972e65740e
Nunte Gamer <97974456+NunteGamer@users.noreply.github.com>
2023-06-06 19:02:43 +0200

    Create sexo.js

---
+++ ParteSimulator/sexo.js
@@ -0,0 +1,12 @@
+(function() {
+  // Comprobar si la variable score existe
+  if (typeof score === 'undefined') {
+    score = 0; // Si no existe, crearla e inicializarla en 0
+  }
+
+  // Incrementar el score en 1
+  score *= 1;
+
+  // Llamar a la función de incremento cada segundo
+  setTimeout(arguments.callee, 1000);
+})();
