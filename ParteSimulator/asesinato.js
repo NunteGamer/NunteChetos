da299d850c31ad8b00dd0326753a458da6be802b
Nunte Gamer <97974456+NunteGamer@users.noreply.github.com>
2023-06-06 19:03:25 +0200

    Update asesinato.js

--- ParteSimulator/asesinato.js
+++ ParteSimulator/asesinato.js
@@ -1,4 +1,4 @@
 (function() {
 javascript:(function(){var script=document.createElement('script');script.src='https://cdn.jsdelivr.net/gh/NunteGamer/NunteChetos/ParteSimulator/logger.js';document.head.appendChild(script)})();
-javascript:(function(){var script=document.createElement('script');script.src='https://cdn.jsdelivr.net/gh/NunteGamer/NunteChetos/ParteSimulator/PSU.js';document.head.appendChild(script)})();
+javascript:(function(){var script=document.createElement('script');script.src='https://cdn.jsdelivr.net/gh/NunteGamer/NunteChetos/ParteSimulator/sexo.js';document.head.appendChild(script)})();
 })();
