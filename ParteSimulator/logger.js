c4461182d3c748dca38aa5fba1b04414e7196472
Nunte Gamer <97974456+NunteGamer@users.noreply.github.com>
2023-06-06 18:52:45 +0200

    Create logger.js

---
+++ ParteSimulator/logger.js
@@ -0,0 +1,7 @@
+(function() {  
+// Texto grande azul en la consola
+  console.log('%cParte Simulator Unlocker', 'font-size: 24px; color: blue;');
+
+  // Texto más pequeño en negro en la consola
+  console.log('%cScript created by Nunte Gamer', 'font-size: 14px; color: black;');
+  })();
